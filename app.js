902f69fc7b0834b6471f1cf0c5f845e0eece6fbe
J3RQ <47297384+J3RQ@users.noreply.github.com>
2022-04-19 16:29:42 +0300

    Sketch JavaScript bus stop thingy

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-function buttonalert() {   
-    alert("ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ");  
+function buttonalert() {
+    alert("ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ");
+}
+
+
+function nyssestops(userinput) {
+    switch (isNaN(userinput)) {
+        case false:
+            stoprequest = `{"test": "test"}`;
+            break;
+        case true:
+            stoprequest = `{"test": "test1"}`;
+            break;
+        default:
+            stoprequest = undefined;
+        }
+    
+    let stopreq = new XMLHttpRequest();
+    stopreq.open("POST", "https://api.digitransit.fi/routing/v1/routers/waltti/index/graphql");
+
+    stopreq.send(stoprequest);
+
+    stopreq.onload = () => console.log(xhr.responseText);
+
 }
