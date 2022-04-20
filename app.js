06ca05473a49b87175c907bfa016d3f770e6e482
J3RQ <47297384+J3RQ@users.noreply.github.com>
2022-04-20 17:27:05 +0300

    Make fetching bus schedules available
    Will iron out the remaining bugs later and make more features

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ function buttonalert() {
 
 function nyssestops(userinput) {
     let stoprequest
-    let querymode 
+    let querymode
     const timestamp = Math.round(Date.now() / 1000);
 
     switch (isNaN(userinput)) {
         case false:
-        querymode = "ID";
-        stoprequest = `query {
+            querymode = "ID";
+            stoprequest = `query {
             stop(id: "tampere:${userinput}") {
                 name
                 gtfsId
@@ -39,8 +39,8 @@ function nyssestops(userinput) {
             }`;
             break;
         case true:
-        querymode = "NAME";
-        stoprequest = `query {
+            querymode = "NAME";
+            stoprequest = `query {
             stops(name: "${userinput}") {
                 name
                 gtfsId
@@ -49,27 +49,94 @@ function nyssestops(userinput) {
                 code
                 }     
             }`;
-        break;
+            break;
         default:
             querymode = undefined;
             stoprequest = undefined;
-        }
-    
+    }
+
     let stopreq = new XMLHttpRequest();
     stopreq.open("POST", "https://api.digitransit.fi/routing/v1/routers/waltti/index/graphql");
     stopreq.setRequestHeader("Content-Type", "application/json");
-    var finalquery = JSON.stringify({"query": stoprequest})
+    var finalquery = JSON.stringify({ "query": stoprequest })
 
     stopreq.send(finalquery);
 
-    stopreq.onload = () => {stopreq.responseText;
-    const stopJSON = JSON.parse(stopreq.responseText);
+    stopreq.onload = () => {
+        stopreq.responseText;
+        const stopJSON = JSON.parse(stopreq.responseText);
 
-    if (querymode == "ID") {
-        console.log(stopJSON["data"]["stop"]["name"]);
-    } else {
-        
-    }
+        function secondsToTime(d) {
+            d = Number(d);
+            var h = Math.floor(d / 3600);
+            var m = Math.floor(d % 3600 / 60);
+            
+            finalh = ""
+            switch (h.toString().length < 2) {
+                case true:
+                    finalh = `0${h}`
+                    break;
+                case false:
+                    finalh = h
+                    break;
+            }
+            finalm = ""
+            switch (m.toString().length < 2) {
+                case true:
+                    finalm = `0${m}`
+                    break;
+                case false:
+                    finalm = m
+                    break;
+            }
+
+            return `${finalh}:${finalm}`
+        }
+
+        if (querymode == "ID") {
+            var resultdiv = document.createElement("div");
+            resultdiv.setAttribute("id", "timetablediv")
+            resultdiv.setAttribute("class", "textdiv")
+            let htmlcontent = ""
+            let arrivalJSON = stopJSON["data"]["stop"]["stoptimesWithoutPatterns"];
+            for (const busJSON in arrivalJSON) {
+                let base = arrivalJSON[busJSON];
+                let destination = base["headsign"]
+                let line = base["trip"]["route"]["shortName"]
+                let timepoint = base["timepoint"]
+                let arrtime
+                let deptime
+                switch (base["realtimeArrival"] < 86400) {
+                    case true:
+                        arrtime = secondsToTime(base["realtimeArrival"])
+                        break;
+                    case false:
+                        arrtime = secondsToTime(base["realtimeArrival"] - 86400)
+                        break;
+                    default:
+                        arrtime = secondsToTime(0)
+                }
+                switch (base["realtimeDeparture"] < 86400) {
+                    case true:
+                        deptime = secondsToTime(base["realtimeDeparture"])
+                        break;
+                    case false:
+                        deptime = secondsToTime(base["realtimeDeparture"] - 86400)
+                        break;
+                    default:
+                        deptime = secondsToTime(0)
+                }
+                base["realtimeDeparture"]
+                let realtime = base["realtimeState"]
+
+                htmlcontent += `Line: ${line}<br>Destination: ${destination}<br>Arrival: ${arrtime}<br>Departure: ${deptime}<br><br>`
+                
+            }
+            resultdiv.innerHTML = htmlcontent
+            document.body.appendChild(resultdiv);
+        } else if (querymode == "NAME") {
+
+        }
 
     }
 
